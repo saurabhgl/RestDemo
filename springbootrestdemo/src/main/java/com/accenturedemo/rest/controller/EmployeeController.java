9e5a81ee6d1e3076eac0cf365b8a7d043b61e781
saura-bh <saura-bh@users.noreply.github.com>
2019-11-25 23:15:02 +0800

    updated controller

--- springbootrestdemo/src/main/java/com/accenturedemo/rest/controller/EmployeeController.java
+++ springbootrestdemo/src/main/java/com/accenturedemo/rest/controller/EmployeeController.java
@@ -1,51 +1,40 @@
 package com.accenturedemo.rest.controller;
 
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.accenturedemo.rest.dao.EmployeeDAO;
 import com.accenturedemo.rest.model.Employee;
 import com.accenturedemo.rest.model.Employees;
 
 @RestController
-@RequestMapping(path = "/employees")
+@RequestMapping(path = "/employees" , produces = MediaType.APPLICATION_JSON_VALUE)
 public class EmployeeController 
 {
     @Autowired
     private EmployeeDAO employeeDao;
     
-    @GetMapping(path="/", produces = "application/json")
+    @GetMapping(path="/")
     public Employees getEmployees() 
     {
         return employeeDao.getAllEmployees();
     }
     
-    @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Object> addEmployee(
-                        @RequestBody Employee employee) throws Exception 
-    {       
-        //Generate resource id
+    @PostMapping(path= "/", consumes = "application/json")
+    public Employees addEmployee(@RequestBody Employee employee)
+    {
+    	 //Generate resource id
         Integer id = employeeDao.getAllEmployees().getEmployeeList().size() + 1;
         employee.setId(id);
         
         //add resource
         employeeDao.addEmployee(employee);
         
-        //Create resource location
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                                    .path("/{id}")
-                                    .buildAndExpand(employee.getId())
-                                    .toUri();
-        
-        //Send location in response
-        return ResponseEntity.created(location).build();
+        return employeeDao.getAllEmployees();
     }
 }

4ea404cdc3bcfb006dee452e7a12758981595d65
saura-bh <saura-bh@users.noreply.github.com>
2019-11-25 20:16:44 +0800

    First commit

---
+++ springbootrestdemo/src/main/java/com/accenturedemo/rest/dao/EmployeeDAO.java
@@ -0,0 +1,28 @@
+package com.accenturedemo.rest.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.accenturedemo.rest.model.Employee;
+import com.accenturedemo.rest.model.Employees;
+
+@Repository
+public class EmployeeDAO 
+{
+    private static Employees list = new Employees();
+    
+    static 
+    {
+        list.getEmployeeList().add(new Employee(1, "Saurabh", "Goel", "saurabhgoel@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Alex", "Das", "abc@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "David", "Gupta", "titanic@gmail.com"));
+    }
+    
+    public Employees getAllEmployees() 
+    {
+        return list;
+    }
+    
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
